971665c909e556b7ea4a3d5dd1be1734f61c3ee6
Mayar Karkout <204641@edu.p.lodz.pl>
2018-05-16 12:21:14 +0200

    Added file with product. Added implementation for reading from file.

--- src/main/java/com/mycompany/salestax/Main.java
+++ src/main/java/com/mycompany/salestax/Main.java
@@ -1,35 +1,76 @@
 package com.mycompany.salestax;
 
-import static com.mycompany.salestax.States.*;
-import java.util.Scanner; 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import static java.lang.Double.parseDouble;
+import java.util.Scanner;
 import java.util.*;
+
 /**
  *
  * @author 204641
  */
 public class Main {
-    
+
     static Scanner input = new Scanner(System.in);
     private static Map<String, Double> netProfitMap = new HashMap<>();
 
-    public static void main(String args[]){
-        System.out.println("Choose your product by number: 1 - Groceries.");
-        if (input.nextInt() == 1) {
-            System.out.println("Input product price:" );
-            Double productionPrice = input.nextDouble();
-            Double overallPrice = States.calculateGroceryPriceForStateWithMaxTax(productionPrice);
-            System.out.printf("Product price in all states: %.2f", overallPrice);
-            for(Map.Entry entry:States.getStatesMap().entrySet()){
-                State state = (State) entry.getValue();
-                Double netProfit = States.calculateProductPrice(productionPrice, state);
-                netProfitMap.put(state.getStateName(), netProfit);
-                System.out.print("\nNet profit in " + entry.getKey());
-                System.out.printf(" is: %.2f", netProfit);
+    public static void main(String args[]) {
+
+        String csvFile = "product_list.csv";
+        String line;
+        Double productionPrice;
+
+        System.out.println("1 - input product data manually.\n"
+                + "2 - read products from file.");
+        int nextInt = input.nextInt();
+        if (nextInt == 1) {
+            System.out.println("Choose your product by number: 1 - Groceries.");
+            if (input.nextInt() == 1) {
+                System.out.println("Input product price:");
+                productionPrice = input.nextDouble();
+                calculateDisplayNetProfitForAllStatesForGroceries(productionPrice);
+            }
+        } else if (nextInt == 2) {
+            try {
+                Scanner scan = new Scanner(new File(csvFile));
+                while (scan.hasNext()) {
+                    line = scan.nextLine();
+                    // use comma as separator
+                    String[] productData = line.split(",", -1);
+                    if (productData[1].trim().equals("groceries")) {
+                        productionPrice = parseDouble(productData[2]);
+                        System.out.println("Product name: " + productData[0]
+                                + ", type:" + productData[1] + ", production price:" + productData[2]);
+                        calculateDisplayNetProfitForAllStatesForGroceries(productionPrice);
+                    } else {
+                        System.out.println("No implementation for " + productData[1] + ".");
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
     public static Map<String, Double> getNetProfitMap() {
         return netProfitMap;
+    }
+
+    public static void calculateDisplayNetProfitForAllStatesForGroceries(Double productionPrice) {
+        Double overallPrice = States.calculateGroceryPriceForStateWithMaxTax(productionPrice);
+        System.out.printf("Product price in all states: %.2f", overallPrice);
+        for (Map.Entry entry : States.getStatesMap().entrySet()) {
+            State state = (State) entry.getValue();
+            Double netProfit = States.calculateProductPrice(productionPrice, state);
+            netProfitMap.put(state.getStateName(), netProfit);
+            System.out.print("\nNet profit in " + entry.getKey());
+            System.out.printf(" is: %.2f", netProfit);
+        }
+        System.out.println("\n");
     }
 }
